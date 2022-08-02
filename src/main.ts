9e5eec507e9bbc63aa4f447d65ee021b8f1ae780
SJY0917032 <sea71511@gmail.com>
2022-08-02 15:09:48 +0900

    Feature : Validation Pipe Add

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './shared/filter/httpExceptionFilter';
@@ -5,7 +6,11 @@ import { setUpSwagger } from './shared/swagger/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  );
   app.useGlobalFilters(new HttpExceptionFilter());
   setUpSwagger(app);
   await app.listen(3000);
