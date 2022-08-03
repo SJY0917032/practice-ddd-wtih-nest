380d789b319b03d9f3e7d070fdbf773a504199f7
SJY0917032 <sea71511@gmail.com>
2022-08-03 14:34:32 +0900

    Feature : Event Entity Add

--- src/events/infra/entity/EventEntity.ts
+++ src/events/infra/entity/EventEntity.ts
@@ -1,1 +1,31 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
 // event Entity
+@Entity('event')
+export class Event {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ type: 'boolean' })
+  exposure: boolean;
+
+  @Column()
+  title: string;
+
+  @Column({ type: 'longtext' })
+  description: string;
+
+  @Column({ type: 'date' })
+  startDate: Date;
+
+  @Column({ type: 'date' })
+  endDate: Date;
+
+  @CreateDateColumn()
+  createdAt: Date;
+}
